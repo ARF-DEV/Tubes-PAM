e2e69e7d6b38c507baf2d8c84ca7c763d186b9f3
PopoSnek <ariefuddinsatriadharma@gmail.com>
2022-05-29 23:09:28 +0700

    feat : add ability to fetch product by name

--- client/src/state/action-creator/index.js
+++ client/src/state/action-creator/index.js
@@ -1,6 +1,6 @@
 // const API_URL = 'http://localhost:8000';
 
-export const getProductList = () => {
+const getProductList = () => {
      return async (dispatch) => {
           try {
                const response = await fetch('http://localhost:8000/products', {
@@ -19,3 +19,25 @@ export const getProductList = () => {
           }
      }
 }
+
+const getProductListByName = (name) => {
+     return async (dispatch) => {
+          try {
+               const response = await fetch(`http://localhost:8000/products?name=${name}`, {
+                    method: 'GET',
+                    headers: {
+                         'Content-Type': 'application/json',
+                    }
+               });
+               const result = await response.json();
+               dispatch({
+                    type: "GET_PRODUCT_LIST",
+                    payload: result
+               })
+          } catch (error) {
+               console.log(error);
+          }
+     }
+}
+
+export default { getProductList, getProductListByName };
