0ff0ddc9c784208edbf7316599bbe008ed783fd7
elizamaharani <elizamaharani@gmail.com>
2022-05-31 01:10:57 +0700

    DescribePageRev2

--- client/src/screen/DescribeScreen.js
+++ client/src/screen/DescribeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView,View, Text, Pressable} from 'react-native';
+import {SafeAreaView,View, Text, Pressable, Image} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from '../style/DescribeScreenStyle';
 
@@ -12,8 +12,19 @@ const DescribeScreen = ({navigation}) => {
                     <Text style={styles.titleText}>Deskripsi Makanan</Text>
                 </Pressable>
             </View>
-            <View>
-                {/* kontennya */}
+            <View style={styles.descriptionCard}>
+                <View>
+                    <Image style={styles.image} source={require('../asset/food2.png')}/>
+                </View>
+                <View style={styles.descriptionText}>
+                    <Text style={styles.menuTitle}>Hola</Text>
+                    <Text style={styles.descText}>Deskripsinya nanti taro disini yang panjang itu </Text>
+                </View>                
+            </View>
+            <View style={styles.theButton}>
+                <Pressable style={styles.button} onPress={()=>console.log('kalo bisa diganti sama modal tulisannya kamu berhasil memesan makanan')}>
+                    <Text style={styles.textButton}>Pesan Makanan</Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     );
