533a7b2e7994d41496dbc944d1b30844eb0eacea
elizamaharani <elizamaharani@gmail.com>
2022-05-31 22:55:03 +0700

    profile

--- client/src/screen/ProfileScreen.js
+++ client/src/screen/ProfileScreen.js
@@ -4,31 +4,46 @@ import {
   Text,
   View,
   Image,
+  SafeAreaView,
 } from 'react-native';
 
 export default class ProfileScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-          <View style={styles.header}>
-            <View style={styles.headerContent}>
-                <Image style={styles.avatar}
-                  source={{uri: 'https://www.its.ac.id/international/wp-content/uploads/sites/66/2020/02/blank-profile-picture-973460_1280.jpg'}}/>
+      <SafeAreaView style={styles.container}>
+          <View style={styles.container2}>
+            <View style={styles.header}>
+              <View style={styles.headerContent}>
+                  <Image style={styles.avatar}
+                    source={{uri: 'https://www.its.ac.id/international/wp-content/uploads/sites/66/2020/02/blank-profile-picture-973460_1280.jpg'}}/>
 
-                <Text style={styles.name}>Gibran</Text>
-                <Text style={styles.userInfo}>Member Platinum </Text>
-                <Text style={styles.userInfo}>Indonesia</Text>
+                  <Text style={styles.name}>Markikan Team</Text>
+                  <Text style={styles.userInfo}>Member Platinum </Text>
+                  <Text style={styles.userInfo}>Indonesia</Text>
+              </View>
             </View>
-          </View>
-          </View>
+            <View style={styles.theTeam}>
+              <Text style={styles.text}>Apps By</Text>
+              <Text style={styles.text2}>Muhammad Ariefuddin</Text>
+              <Text style={styles.text2}>Eliza Maharani</Text>
+              <Text style={styles.text2}>Della Salsabila</Text>
+              <Text style={styles.text2}>Gibran Basyayef</Text>
+            </View>
+        </View>
+      </SafeAreaView>
+      
     );
   }
 }
 
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: "#498A77",
+  container: {
+        backgroundColor:'#498A77',
+        flex: 1,
+  },
+  container2: {
+        margin: 20,
   },
   headerContent:{
     padding:30,
@@ -43,17 +58,35 @@ const styles = StyleSheet.create({
     marginBottom:10,
   },
   name:{
-    fontSize:22,
+    fontSize:30,
     color:"#ffffff",
-    fontWeight:'600',
+    fontFamily: 'Roboto-Bold'
   },
   userInfo:{
     fontSize:16,
     color:"#ffffff",
-    fontWeight:'600',
+    fontFamily: 'Roboto-Medium'
   },
   body:{
     backgroundColor: "#ffff",
     height:500,
   },
+  theTeam:{
+    marginTop: '30%',
+    alignItems: 'center',
+    backgroundColor: '#ffff',
+    borderRadius: 10,
+    padding:10,
+  },
+  text: {
+    fontSize:24,
+    color:'#498A77',
+    fontFamily: 'Roboto-Bold'
+  },
+  text2:{
+    fontSize:16,
+    color:'#498A77',
+    fontFamily: 'Roboto-Regular',
+
+  }
 });
